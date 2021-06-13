c0a5b4defe40a5c76cacc2c4d0389ed3237fbd36
Milos <milosvuksanovic53@gmail.com>
2021-06-13 23:43:11 +1000

    Test

---
+++ src/app/heroes/mock-heros.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:11, name:'Dr Nice', surname:"fffff"},
+    {id:12, name:'Dr fff',surname:"fffff"},
+    {id:13, name:'Dr safda',surname:"fffff"},
+    {id:14, name:'dfds',surname:"fffff"},
+    {id:15, name:'dsf',surname:"fffff"},
+    {id:16, name:'fsdf',surname:"fffff"},
+    {id:17, name:'fff',surname:"fffff"},
+    {id:18, name:'Milo',surname:"fffff"}
+]
